6dd2fb0f5a4e0d27582a7114757dbded61e10532
Sacha Greif <sacha357@gmail.com>
2013-03-18 11:17:38 +0900

    second commit

---
+++ forum.js
@@ -0,0 +1,25 @@
+var Posts = new Meteor.Collection('posts');
+
+if (Meteor.isClient) {
+  Template.posts.helpers({
+    posts: function() {
+      return Posts.find();
+    }
+  })
+
+  Template.form.events = {
+  'click #submit': function(event){
+    event.preventDefault();
+
+    var title = $('#title').val();
+    var body = $('#body').val();
+
+    Posts.insert({
+      title: title,
+      body: body
+    });
+
+    $('#title, #body').val('');
+  }
+}
+}
